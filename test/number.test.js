513bc033428d08c6ec79478593b5b8f023b1aa85
Asbjørn Hegdahl <asbjorn.hegdahl@creuna.no>
2019-01-23 22:36:04 +0100

    Removes comparison tests from number tests

--- test/number.test.js
+++ test/number.test.js
@@ -1,6 +1,6 @@
 import test from "ava";
 
-import { int, float, isEven, isOdd, toFixed } from "../index";
+import { int, float, toFixed } from "../index";
 
 const macro = (t, expected, input) => {
   t.is(expected, input);
@@ -8,9 +8,5 @@ const macro = (t, expected, input) => {
 
 test("int", macro, 1, int("1"));
 test("float", macro, 1.5, float("1.5"));
-test("isEven: true", macro, true, isEven(2));
-test("isEven: false", macro, false, isEven(3));
-test("isOdd: true", macro, true, isOdd(3));
-test("isOdd: false", macro, false, isOdd(2));
 test("toFixed", macro, "1.50", toFixed(2)(1.500001));
 test("toFixed: empty", macro, undefined, toFixed(2)());
